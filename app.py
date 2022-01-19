2a530f072c76597603f3ea96c3ca5386a3750044
Pedro Panza <82601868+Panzaiwnl@users.noreply.github.com>
2022-01-19 01:30:03 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from ast import While
+import PySimpleGUI as sg
+
+# Criando o layout
+
+
+
+def criar_janela_incial():
+    sg.theme('Darkblue4')
+    linha = [
+        [sg.Checkbox(''),sg.Input('')]
+    ]
+   
+    layout = [
+       [sg.Frame('Tarefas', layout=linha,key='container')],
+       [sg.Button('Nova Tarefa'), sg.Button('Resetar')]
+   ]
+   
+    return sg.Window('Todo List', layout=layout, finalize=True)
+
+janela = criar_janela_incial()
+
+
+while True:
+    event, values = janela.read()
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'Nova Tarefa':
+        janela.extend_layout(janela['container'], [[sg.Checkbox('')
+        ,sg.Input('')]])
+    elif event == 'Resetar':
+        janela.close()
+        janela = criar_janela_incial()
